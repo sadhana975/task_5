45a96253281bbc1ef2ba8daada39b2d3575a525e
sadhana975 <maheshwaramsadhana975@gmail.com>
2025-06-30 21:49:55 +0530

    Add files via upload

---
+++ task_5.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE IF NOT EXISTS Customer_db;
+use customer_db;
+
+
+
+-- Customers table
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(50),
+    City VARCHAR(50)
+);
+
+-- Orders table
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    OrderDate DATE,
+    CustomerID INT,
+    Amount DECIMAL(10, 2),
+    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+);
+
+
+
+-- Insert data into Customers
+INSERT INTO Customers VALUES
+(1, 'Alice', 'New York'),
+(2, 'Bob', 'Chicago'),
+(3, 'Charlie', 'Los Angeles'),
+(4, 'David', 'Houston');
+
+-- Insert data into Orders
+INSERT INTO Orders VALUES
+(101, '2024-06-01', 1, 250.00),
+(102, '2024-06-02', 2, 300.00),
+(103, '2024-06-03', 1, 150.00),
+(104, '2024-06-04', 5, 400.00); 
+ -- Note: CustomerID 5 doesn't exist in Customers
+
+SELECT Customers.CustomerName, Orders.OrderID, Orders.Amount
+FROM Customers
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID;
+
+
+
+SELECT Customers.CustomerName, Orders.OrderID, Orders.Amount
+FROM Customers
+RIGHT JOIN Orders ON Customers.CustomerID = Orders.CustomerID;
+
+
+
+SELECT Customers.CustomerName, Orders.OrderID, Orders.Amount
+FROM Customers
+LEFT JOIN Orders ON Customers.CustomerID = Orders.CustomerID
+
+UNION
+
+SELECT Customers.CustomerName, Orders.OrderID, Orders.Amount
+FROM Customers
+RIGHT JOIN Orders ON Customers.CustomerID = Orders.CustomerID;
+
